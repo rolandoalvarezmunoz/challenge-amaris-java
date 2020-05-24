605ef39627c989ad7cdf1059e1ea55797b41b2ef
Rolando Alvarez <rolandoalvarezmunoz1995@gmail.com>
2020-05-24 11:32:25 -0400

    se modifican las clases principales

--- src/User.java
+++ src/User.java
@@ -3,13 +3,27 @@ import java.util.List;
 public class User {
 	private Integer id;
 	private List<Comment> comments;
-
+	
+	public User() {
+		
+	}
 	public User(Integer id, List<Comment> comments) {
+		super();
 		this.id = id;
 		this.comments = comments;
 	}
-
-	public List<Comment> getComments() {
-		return this.comments;
+	public Integer getId() {
+		return id;
 	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public List<Comment> getComments() {
+		return comments;
+	}
+	public void setComments(List<Comment> comments) {
+		this.comments = comments;
+	}
+
+	
 }
